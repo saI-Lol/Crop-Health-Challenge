0f6ad3720e0b6596313b0443ee97f0b8f1f1bbba
ochwo laban <labanochwo@gmail.com>
2025-01-27 21:30:55 +0300

    added changes

---
+++ helper.py
@@ -0,0 +1,62 @@
+import numpy as np
+import rasterio
+
+def process_row_for_features(index, row):
+    features = {'index': index}
+    tif_path = row['tif_path']
+    if not isinstance(tif_path, str):
+        print(f"Skipping entry due to missing tif_path for index {index}")
+        features.update({
+            'ndvi': np.nan,
+            'evi': np.nan,
+            'ndwi': np.nan,
+            'gndvi': np.nan,
+            'savi': np.nan,
+            'msavi': np.nan
+        })
+        return features
+
+    with rasterio.open(tif_path) as src:
+        red = src.read(3)  
+        green = src.read(2) 
+        blue = src.read(1)
+        nir = src.read(4)
+
+        features['ndvi'] = calculate_ndvi(nir, red)
+        features['evi'] = calculate_evi(nir, red, blue)
+        features['ndwi'] = calculate_ndwi(nir, green)
+        features['gndvi'] = calculate_gndvi(nir, green)
+        features['savi'] = calculate_savi(nir, red)
+        features['msavi'] = calculate_msavi(nir, red)
+    return features
+
+
+def calculate_ndvi(nir_band, red_band, epsilon=1e-10):
+    """Calculate NDVI (Normalized Difference Vegetation Index)."""
+    ndvi = (nir_band - red_band) / (nir_band + red_band + epsilon)
+    return np.nanmean(ndvi)
+
+def calculate_evi(nir_band, red_band, blue_band, epsilon=1e-10):
+    """Calculate EVI (Enhanced Vegetation Index)."""
+    evi = 2.5 * (nir_band - red_band) / (nir_band + 6 * red_band - 7.5 * blue_band + 1 + epsilon)
+    return np.nanmean(evi)
+
+def calculate_ndwi(nir_band, green_band, epsilon=1e-10):
+    """Calculate NDWI (Normalized Difference Water Index)."""
+    ndwi = (green_band - nir_band) / (green_band + nir_band + epsilon)
+    return np.nanmean(ndwi)
+
+def calculate_gndvi(nir_band, green_band, epsilon=1e-10):
+    """Calculate GNDVI (Green Normalized Difference Vegetation Index)."""
+    gndvi = (nir_band - green_band) / (nir_band + green_band + epsilon)
+    return np.nanmean(gndvi)
+
+def calculate_savi(nir_band, red_band, L=0.5, epsilon=1e-10):
+    """Calculate SAVI (Soil Adjusted Vegetation Index)."""
+    savi = ((nir_band - red_band) / (nir_band + red_band + L + epsilon)) * (1 + L)
+    return np.nanmean(savi)
+
+def calculate_msavi(nir_band, red_band, epsilon=1e-10):
+    """Calculate MSAVI (Modified Soil Adjusted Vegetation Index)."""
+    msavi = (2 * nir_band + 1 - np.sqrt((2 * nir_band + 1)**2 - 8 * (nir_band - red_band) + epsilon)) / 2
+    return np.nanmean(msavi)
---
+++ tabular.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import numpy as np
+import argparse
+from joblib import Parallel, delayed
+from tqdm import tqdm
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, StratifiedKFold, GroupKFold, KFold
+from helper import process_row_for_features
+
+def main(args):    
+    data_path = args.data_path
+    splitter = args.splitter
+    n_splits = args.n_splits
+    random_state = args.random_state
+    group_col = args.group_col
+    seeds = set()
+    np.random.seed(random_state)
+    while len(seeds) < args.num_seeds:
+        seed = np.random.randint(0, 1000)
+        seeds.add(seed)        
+
+    data = pd.read_csv(data_path)
+
+    new_features = Parallel(n_jobs=-1)(delayed(process_row_for_features)(index, row)
+                                    for index, row in tqdm(data.iterrows(), total=len(data)))
+    new_features_df = pd.DataFrame(new_features).set_index('index')
+    imputer = SimpleImputer(strategy="mean")
+    new_features_df = pd.DataFrame(imputer.fit_transform(new_features_df), columns=new_features_df.columns)
+    data = data.join(new_features_df)
+
+    to_drop = ['geometry', 'tif_path', 'FarmID', "State"]
+    data_combined = data.drop(columns=to_drop).copy()
+
+    category_mapper = {label: idx for idx, label in enumerate(data_combined['category'].unique()) if pd.notna(label)}
+    idx_to_category_mapper = {idx: label for idx, label in enumerate(data_combined['category'].unique()) if pd.notna(label)}
+    data_combined['target'] = data_combined['category'].map(category_mapper)
+    data_combined.drop(columns=['category', 'State'], inplace=True)
+
+    crops = np.unique(data[['Crop', 'CLast', 'CNext']].values)
+    crop_idx_map = {crop:idx for idx, crop in enumerate(crops)}
+    data_combined['Crop'] = data_combined['Crop'].map(crop_idx_map)
+    data_combined['CLast'] = data_combined['CLast'].map(crop_idx_map)
+    data_combined['CNext'] = data_combined['CNext'].map(crop_idx_map)
+    data_combined['CNextMinusCrop'] = data_combined['CNext'] - data_combined['Crop']
+    data_combined['CropMinusCLast'] = data_combined['Crop'] - data_combined['CLast']
+
+    categorical_cols = data_combined.select_dtypes(include=['object']).drop(['dataset'], axis=1).columns
+    for col in categorical_cols:
+        encoder = LabelEncoder()
+        data_combined[col] = encoder.fit_transform(data_combined[col])
+        data_combined[f"{col}_frequency_encoded"] = data_combined[col].map(Counter(data_combined[col]))
+    data_combined = pd.get_dummies(data=data_combined, columns=categorical_cols, dtype='int32')
+
+    train_encoded = data_combined[data_combined['dataset'] == 'train'].reset_index(drop=True)
+    test_encoded = data_combined[data_combined['dataset'] == 'test'].reset_index(drop=True)
+    X = train_encoded.drop(columns=['target', 'dataset'])
+    y = train_encoded['target'].astype(int)
+    
+    if splitter == "tts":
+        for seed in seeds:
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, stratify=y, random_state=random_state)
+            model = LGBMClassifier(verbose=-1, random_state=seed)
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_valid)
+            f1 = f1_score(y_valid, y_pred, average='weighted')
+            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+
+    elif splitter == "skf":
+        for seed in seeds:
+            skf = StratifiedKFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+            results = {'y_true': [], 'y_pred': []}
+            for train_idx, valid_idx in skf.split(X, y):
+                X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
+                y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
+                model = LGBMClassifier(verbose=-1, random_state=seed)
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_valid)
+                results['y_true'].extend(y_valid)
+                results['y_pred'].extend(y_pred)
+            f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
+            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+
+    elif splitter == "gkf":
+        for seed in seeds:
+            gkf = GroupKFold(n_splits=n_splits)
+            results = {'y_true': [], 'y_pred': []}
+            for train_idx, valid_idx in gkf.split(X, y, groups=X[group_col]):
+                X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
+                y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
+                model = LGBMClassifier(verbose=-1, random_state=seed)
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_valid)
+                results['y_true'].extend(y_valid)
+                results['y_pred'].extend(y_pred)
+            f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
+            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+
+    elif splitter == "kf":        
+        for seed in seeds:
+            kf = KFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+            results = {'y_true': [], 'y_pred': []}
+            for train_idx, valid_idx in kf.split(X):
+                X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
+                y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
+                model = LGBMClassifier(verbose=-1, random_state=seed)
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_valid)
+                results['y_true'].extend(y_valid)
+                results['y_pred'].extend(y_pred)
+            f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
+            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Telangana Crop Health Challenge')
+    parser.add_argument('--data_path', type=str, default="/kaggle/input/sentineltimeseriesdata/SentinelTimeSeriesData/Data.csv", help='Path to the data CSV file')
+    parser.add_argument("--splitter", type=str, default="tts", help="Splitter to use for cross-validation")
+    parser.add_argument("--n_splits", type=int, default=None, help="Number of splits for cross-validation")
+    parser.add_argument("--random_state", type=int, default=42, help="Random state for reproducibility")
+    parser.add_argument("--group_col", type=str, default=None, help="Column to use for GroupKFold")
+    parser.add_argument("--num_seeds", type=int, default = 1, help="Number of seeds to use for cross-validation")
+    args = parser.parse_args()
+    if args.splitter not in ["skf", "gkf", "tts", "kf"]:
+        raise ValueError("Splitter should be one of ['skf', 'gkf', 'tts', 'kf']")
+    if args.splitter in ["skf", "gkf", "kf"] and args.n_splits is None:
+        raise ValueError("Number of splits should be provided")    
+    if args.splitter in ["gkf"] and args.group_col is None:
+        raise ValueError("Group column should be provided for GroupKFold")
+    main(args)
