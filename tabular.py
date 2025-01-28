e582f655bfeb0d68117f31aa5ccd6ede2acd68ac
ochwo laban <labanochwo@gmail.com>
2025-01-28 09:15:25 +0300

    added changes

--- tabular.py
+++ tabular.py
@@ -107,15 +107,15 @@ def main(args):
                     # encoder.fit(X_temp[col])
                     # X_valid[col] = encoder.transform(X_valid[col])
                     # X_train[col] = encoder.transform(X_train[col])
-                    X_train = X_train.drop(columns=[col], axis=1)
-                    X_valid = X_valid.drop(columns=[col], axis=1)
+                    # X_train = X_train.drop(columns=[col], axis=1)
+                    # X_valid = X_valid.drop(columns=[col], axis=1)
 
-                # X_train['dataset'] = 'train'
-                # X_valid['dataset'] = 'valid'
-                # X_temp = pd.concat([X_train, X_valid])
-                # X_temp = pd.get_dummies(X_temp, columns=categorical_cols, dtype='int32')
-                # X_train = X_temp[X_temp['dataset'] == 'train'].drop(columns=['dataset'], axis=1)
-                # X_valid = X_temp[X_temp['dataset'] == 'valid'].drop(columns=['dataset'], axis=1)
+                X_train['dataset'] = 'train'
+                X_valid['dataset'] = 'valid'
+                X_temp = pd.concat([X_train, X_valid])
+                X_temp = pd.get_dummies(X_temp, columns=categorical_cols, dtype='int32')
+                X_train = X_temp[X_temp['dataset'] == 'train'].drop(columns=['dataset'], axis=1)
+                X_valid = X_temp[X_temp['dataset'] == 'valid'].drop(columns=['dataset'], axis=1)
 
                 #get min and max values in dfs
                 # print(X_train.min().min(), X_train.max().max())
