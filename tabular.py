819a0a61b5a173834760f90bbeceef566e7ac047
ochwo laban <labanochwo@gmail.com>
2025-01-28 12:23:00 +0300

    added changes

--- tabular.py
+++ tabular.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import argparse
-from joblib import Parallel, delayed
+# from joblib import Parallel, delayed
 from tqdm import tqdm
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
@@ -13,21 +13,79 @@ from lightgbm import LGBMClassifier
 from sklearn.metrics import f1_score
 from shapely.wkt import loads
 from itertools import combinations
+# import wandb
+
+def encode_data(X_train, X_valid, y_train, y_valid, encs):
+    X_train_encoded_list = []
+    X_valid_encoded_list = []
+    for enc in encs:
+        if enc == "frequency":
+            for col in X_train.columns:
+                freq = X_train[col].value_counts().to_dict()
+                X_train_encoded_list.append(X_train[col].map(freq))
+                X_valid_encoded_list.append(X_valid[col].map(freq))
+        if enc == "label": 
+            X_train_temp = X_train.copy()   
+            X_valid_temp = X_valid.copy()       
+            X_train_temp['dataset'] = 'train'
+            X_valid_temp['dataset'] = 'valid'
+            X = pd.concat([X_train_temp, X_valid_temp], axis=0)
+            for col in X_train.columns:
+                le = LabelEncoder()                
+                X[col] = le.fit_transform(X[col])
+            X_train_encoded_list.append(X[X['dataset'] == 'train'].drop('dataset', axis=1))
+            X_valid_encoded_list.append(X[X['dataset'] == 'valid'].drop('dataset', axis=1))
+        
+        train_temp = pd.concat([X_train, y_train], axis=1)
+        valid_temp = pd.concat([X_valid, y_valid], axis=1)
+        if enc == "target_mean":
+            for col in X_train.columns:
+                target_mean = train.groupby(col)['target'].mean().to_dict()
+                X_train_encoded_list.append(X_train[col].map(target_mean))
+                X_valid_encoded_list.append(X_valid[col].map(target_mean))
+        if enc == "target_median":
+            for col in X_train.columns:
+                target_median = train.groupby(col)['target'].median().to_dict()
+                X_train_encoded_list.append(X_train[col].map(target_median))
+                X_valid_encoded_list.append(X_valid[col].map(target_median))
+        if enc == "target_min":
+            for col in X_train.columns:
+                target_min = train.groupby(col)['target'].min().to_dict()
+                X_train_encoded_list.append(X_train[col].map(target_min))
+                X_valid_encoded_list.append(X_valid[col].map(target_min))
+        if enc == "target_max":
+            for col in X_train.columns:
+                target_max = train.groupby(col)['target'].max().to_dict()
+                X_train_encoded_list.append(X_train[col].map(target_max))
+                X_valid_encoded_list.append(X_valid[col].map(target_max))
+        if enc == "target_std":
+            for col in X_train.columns:
+                target_std = train.groupby(col)['target'].std().to_dict()
+                X_train_encoded_list.append(X_train[col].map(target_std))
+                X_valid_encoded_list.append(X_valid[col].map(target_std))            
+            
+    
+    X_train_encoded = pd.concat(X_train_encoded_list, axis=1)
+    X_valid_encoded = pd.concat(X_valid_encoded_list, axis=1)
+    return X_train_encoded, X_valid_encoded, y_train, y_valid
+
 
 
 def main(args):    
+    # wandb.init(project="telangana-crop-health", name="Experiment-1")
     data_path = args.data_path
-    splitter = args.splitter
+    # splitter = args.splitter
     n_splits = args.n_splits
     random_state = args.random_state
-    group_col = args.group_col
-    seeds = set()
-    np.random.seed(random_state)
-    while len(seeds) < args.num_seeds:
-        seed = np.random.randint(0, 1000)
-        seeds.add(seed)        
+    # group_col = args.group_col
+    # seeds = set()
+    # np.random.seed(random_state)
+    # while len(seeds) < args.num_seeds:
+    #     seed = np.random.randint(0, 1000)
+    #     seeds.add(seed)        
 
     data = pd.read_csv(data_path)
+    data = data[data['dataset'] == 'train'].reset_index(drop=True)
     # data['geometry'] = data['geometry'].apply(loads)
     # data[['min_x', 'min_y', 'max_x', 'max_y']] = data.apply(
     #     lambda row: pd.Series(row['geometry'].bounds),
@@ -41,8 +99,8 @@ def main(args):
     # new_features_df = pd.DataFrame(imputer.fit_transform(new_features_df), columns=new_features_df.columns)
     # data = data.join(new_features_df)
 
-    to_drop = ['geometry', 'tif_path', 'FarmID', "State", "SDate", "HDate"]
-    data_combined = data.drop(columns=to_drop).copy()
+    to_drop = ['geometry', 'tif_path', 'FarmID', "State", "SDate", "HDate", "dataset"]
+    data_combined = data.drop(columns=to_drop)
 
     category_mapper = {label: idx for idx, label in enumerate(data_combined['category'].unique()) if pd.notna(label)}
     idx_to_category_mapper = {idx: label for idx, label in enumerate(data_combined['category'].unique()) if pd.notna(label)}
@@ -52,12 +110,50 @@ def main(args):
     categorical_cols = ['Crop', 'District', 'Sub-District', 'CropCoveredArea', 
     'CNext', 'CLast', 'CTransp', 'IrriType', 
     'IrriSource', 'IrriCount', 'WaterCov', 'ExpYield', 'Season']
-    data_combined = data_combined[categorical_cols]
+    encoding = ["frequency", "label", "target_mean", "target_median", "target_std", "target_min", "target_max"]
+    data_combined = data_combined[categorical_cols + ['target']].copy()
 
     column_combinations = []
+    encoding_combinations = []
     for i in range(1, len(categorical_cols)+1):
         column_combinations.extend(combinations(categorical_cols, i))
-    print(len(column_combinations))
+    
+    for i in range(1, len(encoding)+1):
+        encoding_combinations.extend(combinations(encoding, i))
+    
+    for cols in column_combinations:
+        df_cols = data_combined[list(cols) + ['target']].copy()
+        if len(cols) > 1:
+            df_cols['combined'] = df_cols[cols].apply(lambda row: '_'.join(row.values.astype(str)), axis=1)
+        cols = list(df_cols.columns)
+        
+        for encs in encoding_combinations:
+            encs = list(encs)
+            df = df_cols.copy()
+            X = df.drop(columns=['target'], axis=1)
+            y = df['target']
+            skf = StratifiedKFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+            results = {'y_true': [], 'y_pred': []}
+            for train_idx, valid_idx in skf.split(X, y):
+                X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
+                y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
+                X_train_encoded, X_valid_encoded, y_train, y_valid = encode_data(X_train, X_valid, y_train, y_valid, encs)
+                model = LGBMClassifier(verbose=-1, random_state=seed)
+                model.fit(X_train_encoded, y_train)
+                y_pred = model.predict(X_valid_encoded)
+                results['y_true'].extend(y_valid)
+                results['y_pred'].extend(y_pred)
+            f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
+            print(f"Features: {cols}\nEncoding: {encs}\nF1 Score: {f1}\n")
+            # wandb.log({
+            #     "features": cols,
+            #     "encoding": encs,
+            #     "f1": f1
+            # })
+
+
+
+
 
 
     # crops = np.unique(data[['Crop', 'CLast', 'CNext']].values)
@@ -82,17 +178,16 @@ def main(args):
     # X = train_encoded.drop(columns=['target', 'dataset'])
     # y = train_encoded['target'].astype(int)
     
-    if splitter == "tts":
-        for seed in seeds:
-            X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, stratify=y, random_state=random_state)
-            model = LGBMClassifier(verbose=-1, random_state=seed)
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_valid)
-            f1 = f1_score(y_valid, y_pred, average='weighted')
-            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+    # if splitter == "tts":
+    #     for seed in seeds:
+    #         X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, stratify=y, random_state=random_state)
+    #         model = LGBMClassifier(verbose=-1, random_state=seed)
+    #         model.fit(X_train, y_train)
+    #         y_pred = model.predict(X_valid)
+    #         f1 = f1_score(y_valid, y_pred, average='weighted')
+    #         print(f"\nWeighted F1 Score for seed {seed}:", f1)
 
-    elif splitter == "skf":
-        pass
+    # elif splitter == "skf":
         # for seed in seeds:
         #     skf = StratifiedKFold(n_splits=n_splits, random_state=random_state, shuffle=True)
         #     results = {'y_true': [], 'y_pred': []}
@@ -107,50 +202,50 @@ def main(args):
         #     f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
         #     print(f"\nWeighted F1 Score for seed {seed}:", f1)
 
-    elif splitter == "gkf":
-        for seed in seeds:
-            gkf = GroupKFold(n_splits=n_splits)
-            results = {'y_true': [], 'y_pred': []}
-            for train_idx, valid_idx in gkf.split(X, y, groups=X[group_col]):
-                X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
-                y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
-                model = LGBMClassifier(verbose=-1, random_state=seed)
-                model.fit(X_train, y_train)
-                y_pred = model.predict(X_valid)
-                results['y_true'].extend(y_valid)
-                results['y_pred'].extend(y_pred)
-            f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
-            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+    # elif splitter == "gkf":
+    #     for seed in seeds:
+    #         gkf = GroupKFold(n_splits=n_splits)
+    #         results = {'y_true': [], 'y_pred': []}
+    #         for train_idx, valid_idx in gkf.split(X, y, groups=X[group_col]):
+    #             X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
+    #             y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
+    #             model = LGBMClassifier(verbose=-1, random_state=seed)
+    #             model.fit(X_train, y_train)
+    #             y_pred = model.predict(X_valid)
+    #             results['y_true'].extend(y_valid)
+    #             results['y_pred'].extend(y_pred)
+    #         f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
+    #         print(f"\nWeighted F1 Score for seed {seed}:", f1)
 
-    elif splitter == "kf":        
-        for seed in seeds:
-            kf = KFold(n_splits=n_splits, random_state=random_state, shuffle=True)
-            results = {'y_true': [], 'y_pred': []}
-            for train_idx, valid_idx in kf.split(X):
-                X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
-                y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
-                model = LGBMClassifier(verbose=-1, random_state=seed)
-                model.fit(X_train, y_train)
-                y_pred = model.predict(X_valid)
-                results['y_true'].extend(y_valid)
-                results['y_pred'].extend(y_pred)
-            f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
-            print(f"\nWeighted F1 Score for seed {seed}:", f1)
+    # elif splitter == "kf":        
+    #     for seed in seeds:
+    #         kf = KFold(n_splits=n_splits, random_state=random_state, shuffle=True)
+    #         results = {'y_true': [], 'y_pred': []}
+    #         for train_idx, valid_idx in kf.split(X):
+    #             X_train, X_valid = X.loc[train_idx], X.loc[valid_idx]
+    #             y_train, y_valid = y.loc[train_idx], y.loc[valid_idx]
+    #             model = LGBMClassifier(verbose=-1, random_state=seed)
+    #             model.fit(X_train, y_train)
+    #             y_pred = model.predict(X_valid)
+    #             results['y_true'].extend(y_valid)
+    #             results['y_pred'].extend(y_pred)
+    #         f1 = f1_score(results['y_true'], results['y_pred'], average='weighted')
+    #         print(f"\nWeighted F1 Score for seed {seed}:", f1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Telangana Crop Health Challenge')
     parser.add_argument('--data_path', type=str, default="/kaggle/input/sentineltimeseriesdata/SentinelTimeSeriesData/Data.csv", help='Path to the data CSV file')
-    parser.add_argument("--splitter", type=str, default="tts", help="Splitter to use for cross-validation")
+    # parser.add_argument("--splitter", type=str, default="tts", help="Splitter to use for cross-validation")
     parser.add_argument("--n_splits", type=int, default=5, help="Number of splits for cross-validation")
     parser.add_argument("--random_state", type=int, default=42, help="Random state for reproducibility")
-    parser.add_argument("--group_col", type=str, default="Crop", help="Column to use for GroupKFold")
-    parser.add_argument("--num_seeds", type=int, default = 1, help="Number of seeds to use for cross-validation")
+    # parser.add_argument("--group_col", type=str, default="Crop", help="Column to use for GroupKFold")
+    # parser.add_argument("--num_seeds", type=int, default = 1, help="Number of seeds to use for cross-validation")
     args = parser.parse_args()
-    if args.splitter not in ["skf", "gkf", "tts", "kf"]:
-        raise ValueError("Splitter should be one of ['skf', 'gkf', 'tts', 'kf']")
-    if args.splitter in ["skf", "gkf", "kf"] and args.n_splits is None:
-        raise ValueError("Number of splits should be provided")    
-    if args.splitter in ["gkf"] and args.group_col is None:
-        raise ValueError("Group column should be provided for GroupKFold")
+    # if args.splitter not in ["skf", "gkf", "tts", "kf"]:
+    #     raise ValueError("Splitter should be one of ['skf', 'gkf', 'tts', 'kf']")
+    # if args.splitter in ["skf", "gkf", "kf"] and args.n_splits is None:
+    #     raise ValueError("Number of splits should be provided")    
+    # if args.splitter in ["gkf"] and args.group_col is None:
+    #     raise ValueError("Group column should be provided for GroupKFold")
     main(args)
